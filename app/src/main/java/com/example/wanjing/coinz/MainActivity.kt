2ce182ebd24cdc4254d9a4b8ca313bf7ca7e5124
WanjingChen <wanjing@wanjingchendemacbook-pro.local>
2018-10-22 19:36:03 +0100

    Successfully installed Mapbox.

--- app/src/main/java/com/example/wanjing/coinz/MainActivity.kt
+++ app/src/main/java/com/example/wanjing/coinz/MainActivity.kt
@@ -2,11 +2,60 @@ package com.example.wanjing.coinz
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import com.mapbox.mapboxsdk.Mapbox
+import com.mapbox.mapboxsdk.maps.MapView
+
+
+
 
 class MainActivity : AppCompatActivity() {
 
+    private var mapView: MapView? = null
+
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        Mapbox.getInstance(this, "pk.eyJ1Ijoid2FuamluZyIsImEiOiJjam14aHFzaXYwZGxmM3B0Y2xiMTV5ZDN3In0.mWGG-pa6gzJEisINLaDLRA");
         setContentView(R.layout.activity_main)
+        mapView = findViewById(R.id.mapView);
+        mapView?.onCreate(savedInstanceState);
+
     }
+
+    public override fun onStart() {
+        super.onStart()
+        mapView?.onStart()
+    }
+
+    public override fun onResume() {
+        super.onResume()
+        mapView?.onResume()
+    }
+
+    public override fun onPause() {
+        super.onPause()
+        mapView?.onPause()
+    }
+
+    public override fun onStop() {
+        super.onStop()
+        mapView?.onStop()
+    }
+
+    override fun onLowMemory() {
+        super.onLowMemory()
+        mapView?.onLowMemory()
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        mapView?.onDestroy()
+    }
+
+    override fun onSaveInstanceState(outState: Bundle) {
+        super.onSaveInstanceState(outState)
+        mapView?.onSaveInstanceState(outState)
+    }
+
 }
