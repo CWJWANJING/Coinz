d4ac61ecdf2b4d3af84b6eea1f599745dd203ca3
CWJWANJING <wanjingchencwj@gmail.com>
2018-11-13 12:19:00 +0000

    Followed Kotlin youtube tutorial, 'getting user location'.

--- app/src/main/java/com/example/wanjing/coinz/MainActivity.kt
+++ app/src/main/java/com/example/wanjing/coinz/MainActivity.kt
@@ -2,16 +2,34 @@ package com.example.wanjing.coinz
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import com.mapbox.android.core.permissions.PermissionsManager
 import com.mapbox.mapboxsdk.Mapbox
 import com.mapbox.mapboxsdk.maps.MapView
+import com.mapbox.mapboxsdk.maps.MapboxMap
+import android.location.Location
+import com.mapbox.android.core.location.LocationEngine
+import com.mapbox.android.core.location.LocationEngineListener
+import com.mapbox.android.core.location.LocationEnginePriority
+import com.mapbox.android.core.location.LocationEngineProvider
+import com.mapbox.android.core.permissions.PermissionsListener
+import com.mapbox.geojson.Point
+import com.mapbox.mapboxsdk.camera.CameraUpdateFactory
+import com.mapbox.mapboxsdk.geometry.LatLng
+import com.mapbox.mapboxsdk.location.modes.CameraMode
+import com.mapbox.mapboxsdk.location.modes.RenderMode
+import com.mapbox.mapboxsdk.plugins.locationlayer.LocationLayerPlugin
 
 
+class MainActivity : AppCompatActivity(), PermissionsListener, LocationEngineListener {
 
 
-class MainActivity : AppCompatActivity() {
+    private lateinit var mapView: MapView
+    private lateinit var map: MapboxMap
+    private lateinit var permissionManager: PermissionsManager
+    private lateinit var originLocation: Location
 
-    private var mapView: MapView? = null
-
+    private var locationEngine: LocationEngine? = null
+    private var locationLayerPlugin: LocationLayerPlugin? = null
 
 
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -20,7 +38,80 @@ class MainActivity : AppCompatActivity() {
         setContentView(R.layout.activity_main)
         mapView = findViewById(R.id.mapView);
         mapView?.onCreate(savedInstanceState);
+        mapView.getMapAsync { mapboxMap ->
+            map = mapboxMap
+            enableLocation()
+        }
 
+    }
+
+    private fun enableLocation() {
+        if (PermissionsManager.areLocationPermissionsGranted(this)) {
+            initializeLocationEngine()
+            initializeLocationLayer()
+
+        } else {
+            permissionManager = PermissionsManager(this)
+            permissionManager.requestLocationPermissions(this)
+
+        }
+
+    }
+
+    @SuppressWarnings("MissingPermission")
+    private fun initializeLocationEngine() {
+        locationEngine = LocationEngineProvider(this).obtainBestLocationEngineAvailable()
+        locationEngine?.priority = LocationEnginePriority.HIGH_ACCURACY
+        locationEngine?.activate()
+
+        val lastLocation = locationEngine?.lastLocation
+        if (lastLocation != null) {
+            originLocation = lastLocation
+            setCameraPosition(lastLocation)
+        } else {
+            locationEngine?.addLocationEngineListener(this)
+        }
+    }
+
+    private fun initializeLocationLayer() {
+        locationLayerPlugin = LocationLayerPlugin(mapView, map, locationEngine)
+        locationLayerPlugin?.isLocationLayerEnabled = true
+        locationLayerPlugin?.cameraMode = CameraMode.TRACKING
+        locationLayerPlugin?.renderMode = RenderMode.NORMAL
+
+    }
+
+    private fun setCameraPosition(location: Location) {
+        map.animateCamera(CameraUpdateFactory.newLatLngZoom(
+                LatLng(location.latitude, location.longitude), 13.0))
+
+    }
+
+    override fun onExplanationNeeded(permissionsToExplain: MutableList<String>?) {
+        // present a toast or a dialog to give reasons why they need to grant access
+        TODO("not implemented")
+    }
+
+    override fun onPermissionResult(granted: Boolean) {
+        if(granted) {
+            enableLocation()
+        }
+    }
+
+    override fun onRequestPermissionsResult(requestCode: Int, permissions: Array<out String>, grantResults: IntArray) {
+        permissionManager.onRequestPermissionsResult(requestCode, permissions, grantResults)
+    }
+
+    override fun onLocationChanged(location: Location?) {
+        location?.let {
+            originLocation = location
+            setCameraPosition(location)
+        }
+    }
+
+    @SuppressWarnings("MissingPermission")
+    override fun onConnected() {
+        locationEngine?.requestLocationUpdates()
     }
 
     public override fun onStart() {
