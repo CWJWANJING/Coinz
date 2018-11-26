7244b12f4e095e02649f64d7f7d477b39daee2c5
CWJWANJING <wanjingchencwj@gmail.com>
2018-11-26 15:12:20 +0000

    Followed Kotlin youtube tutorial, 'adding markers to the map'.

--- app/src/main/java/com/example/wanjing/coinz/MainActivity.kt
+++ app/src/main/java/com/example/wanjing/coinz/MainActivity.kt
@@ -1,5 +1,6 @@
 package com.example.wanjing.coinz
 
+import android.annotation.SuppressLint
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
 import com.mapbox.android.core.permissions.PermissionsManager
@@ -7,12 +8,22 @@ import com.mapbox.mapboxsdk.Mapbox
 import com.mapbox.mapboxsdk.maps.MapView
 import com.mapbox.mapboxsdk.maps.MapboxMap
 import android.location.Location
+import android.widget.Button
 import com.mapbox.android.core.location.LocationEngine
 import com.mapbox.android.core.location.LocationEngineListener
 import com.mapbox.android.core.location.LocationEnginePriority
 import com.mapbox.android.core.location.LocationEngineProvider
 import com.mapbox.android.core.permissions.PermissionsListener
 import com.mapbox.geojson.Point
+import com.mapbox.geojson.Feature
+import com.mapbox.geojson.FeatureCollection
+import com.mapbox.geojson.Geometry
+import com.google.gson.JsonObject
+import com.mapbox.mapboxsdk.style.sources.GeoJsonSource
+import com.mapbox.mapboxsdk.annotations.Icon
+import com.mapbox.mapboxsdk.annotations.IconFactory
+import com.mapbox.mapboxsdk.annotations.Marker
+import com.mapbox.mapboxsdk.annotations.MarkerOptions
 import com.mapbox.mapboxsdk.camera.CameraUpdateFactory
 import com.mapbox.mapboxsdk.geometry.LatLng
 import com.mapbox.mapboxsdk.location.modes.CameraMode
@@ -20,16 +31,21 @@ import com.mapbox.mapboxsdk.location.modes.RenderMode
 import com.mapbox.mapboxsdk.plugins.locationlayer.LocationLayerPlugin
 
 
-class MainActivity : AppCompatActivity(), PermissionsListener, LocationEngineListener {
+class MainActivity : AppCompatActivity(), PermissionsListener, LocationEngineListener, MapboxMap.OnMapClickListener {
 
 
+    private val tag = "MainActivity"
     private lateinit var mapView: MapView
     private lateinit var map: MapboxMap
+    private lateinit var start_button: Button
     private lateinit var permissionManager: PermissionsManager
     private lateinit var originLocation: Location
+    private lateinit var originPosition: Point
+    private lateinit var destinationPosition: Point
 
     private var locationEngine: LocationEngine? = null
     private var locationLayerPlugin: LocationLayerPlugin? = null
+    private var destinationMarker: Marker? = null
 
 
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -37,12 +53,16 @@ class MainActivity : AppCompatActivity(), PermissionsListener, LocationEngineLis
         Mapbox.getInstance(this, "pk.eyJ1Ijoid2FuamluZyIsImEiOiJjam14aHFzaXYwZGxmM3B0Y2xiMTV5ZDN3In0.mWGG-pa6gzJEisINLaDLRA");
         setContentView(R.layout.activity_main)
         mapView = findViewById(R.id.mapView);
+        start_button = findViewById(R.id.start_button)
         mapView?.onCreate(savedInstanceState);
         mapView.getMapAsync { mapboxMap ->
             map = mapboxMap
             enableLocation()
         }
 
+        start_button.setOnClickListener{
+
+        }
     }
 
     private fun enableLocation() {
@@ -87,6 +107,17 @@ class MainActivity : AppCompatActivity(), PermissionsListener, LocationEngineLis
 
     }
 
+    @SuppressLint("ResourceAsColor")
+    override fun onMapClick(point: LatLng) {
+        destinationMarker = map.addMarker(MarkerOptions().position(point))
+        destinationPosition = Point.fromLngLat(point.longitude,point.latitude)
+        originPosition = Point.fromLngLat(originLocation.longitude,originLocation.latitude)
+
+
+        start_button.isEnabled = true
+        start_button.setBackgroundColor(R.color.mapbox_blue)
+    }
+
     override fun onExplanationNeeded(permissionsToExplain: MutableList<String>?) {
         // present a toast or a dialog to give reasons why they need to grant access
         TODO("not implemented")
@@ -116,37 +147,37 @@ class MainActivity : AppCompatActivity(), PermissionsListener, LocationEngineLis
 
     public override fun onStart() {
         super.onStart()
-        mapView?.onStart()
+        mapView.onStart()
     }
 
     public override fun onResume() {
         super.onResume()
-        mapView?.onResume()
+        mapView.onResume()
     }
 
     public override fun onPause() {
         super.onPause()
-        mapView?.onPause()
+        mapView.onPause()
     }
 
     public override fun onStop() {
         super.onStop()
-        mapView?.onStop()
+        mapView.onStop()
     }
 
     override fun onLowMemory() {
         super.onLowMemory()
-        mapView?.onLowMemory()
+        mapView.onLowMemory()
     }
 
     override fun onDestroy() {
         super.onDestroy()
-        mapView?.onDestroy()
+        mapView.onDestroy()
     }
 
     override fun onSaveInstanceState(outState: Bundle) {
         super.onSaveInstanceState(outState)
-        mapView?.onSaveInstanceState(outState)
+        mapView.onSaveInstanceState(outState)
     }
 
 }
