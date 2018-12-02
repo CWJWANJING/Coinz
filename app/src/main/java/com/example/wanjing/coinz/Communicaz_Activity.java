036c3336bf5c8b62df28064ac6b904c70b8f5ebe
CWJWANJING <wanjingchencwj@gmail.com>
2018-12-02 20:08:10 +0000

    Nothing much progressed...I'm really anxious about this project... Got this error: 'com.google.gson.JsonSyntaxException: java.lang.IllegalStateException: Expected BEGIN_OBJECT but was STRING at line 1 column 1 path $' Don't know why this occurs...

---
+++ app/src/main/java/com/example/wanjing/coinz/Communicaz_Activity.java
@@ -0,0 +1,87 @@
+//package com.example.wanjing.coinz;
+//
+//import android.support.v7.app.AppCompatActivity;
+//import android.os.Bundle;
+//import android.support.v7.widget.Toolbar;
+//import android.util.Log;
+//import android.view.View;
+//import android.widget.Toast;
+//
+//import com.google.firebase.firestore.DocumentReference;
+//import com.google.firebase.firestore.FirebaseFirestore;
+//import com.google.firebase.firestore.FirebaseFirestoreSettings;
+//
+//import java.util.HashMap;
+//import java.util.Map;
+//
+//public class Communicaz_Activity extends AppCompatActivity {
+//
+//
+//    private static final String TAG = "JKommunicatorActivity";
+//    private static final String COLLECTION_KEY = "Chat";
+//    private static final String DOCUMENT_KEY = "Message";
+//    private static final String NAME_FIELD = "Name";
+//    private static final String TEXT_FIELD = "Text";
+//
+//    private FirebaseFirestore firestore;
+//    private DocumentReference firestoreChat;
+//
+//
+//    @Override
+//    protected void onCreate(Bundle savedInstanceState) {
+//        super.onCreate(savedInstanceState);
+//        setContentView(R.layout.activity_communicaz_);
+//
+//        name_text = findViewById(R.id.jname_text);
+//        outgoing_message_text = findViewById(R.id.joutgoing_message_text);
+//        incoming_message_text = findViewById(R.id.jincoming_message_text);
+//// Set up the toolbar at the top of the view
+//        Toolbar toolbar = findViewById(R.id.jtoolbar);
+//        setSupportActionBar(toolbar);
+//// Program the ”Send message” button
+//        FloatingActionButton send_message_button = findViewById(R.id.jsend_message_button);
+//        send_message_button.setOnClickListener(view -> sendMessage());
+//
+//        firestore = FirebaseFirestore.getInstance();
+//// Use com.google.firebase.Timestamp objects instead of java.util.Date objects
+//        FirebaseFirestoreSettings settings = new FirebaseFirestoreSettings.Builder().setTimestampsInSnapshotsEnabled(true)
+//                .build();
+//        firestore.setFirestoreSettings(settings);
+//        firestoreChat =
+//                firestore.collection(COLLECTION_KEY)
+//                        .document(DOCUMENT_KEY); // ”Message”
+//// Set a listener for changes to the /Chat/Message document
+//        realtimeUpdateListener();
+//    }
+//
+//    private void sendMessage() {
+//// create a message of the form { ”Name”: str1, ”Text”: str2 }
+//        Map<String, String> newMessage = new HashMap<>();
+//        newMessage.put(NAME_FIELD, name_text.getText().toString());
+//        newMessage.put(TEXT_FIELD, outgoing_message_text.getText().toString());
+//// send the message and listen for success or failure
+//        firestoreChat.set(newMessage)
+//                .addOnSuccessListener(v -> Toast.makeText(getApplicationContext(),
+//
+//                        "Message sent!",
+//                        Toast.LENGTH_SHORT).show())
+//                .addOnFailureListener(e -> {
+//                    Log.e(TAG, e.getMessage());
+//                });
+//
+//    }
+//
+//    private void realtimeUpdateListener() {
+//        firestoreChat.addSnapshotListener((documentSnapshot, e) -> {
+//            if (e != null) {
+//                Log.e(TAG, e.getMessage());
+//            } else if (documentSnapshot != null && documentSnapshot.exists()) {
+//                String incoming = (documentSnapshot.getData().get(NAME_FIELD))
+//                        + ": "
+//                        + (documentSnapshot.getData().get(TEXT_FIELD));
+//                incoming_message_text.setText(incoming);
+//            }
+//        });
+//    }
+//
+//}
--- app/src/main/java/com/example/wanjing/coinz/MainActivity.java
+++ app/src/main/java/com/example/wanjing/coinz/MainActivity.java
@@ -1,18 +1,44 @@
 package com.example.wanjing.coinz;
 
+import android.annotation.SuppressLint;
+import android.content.Context;
+import android.content.Intent;
+import android.content.SharedPreferences;
+import android.icu.text.SimpleDateFormat;
+import android.icu.util.Calendar;
 import android.location.Location;
+
+import android.net.Uri;
+import android.os.AsyncTask;
+import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.Toolbar;
 import android.util.Log;
 
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.ActionCodeSettings;
+import com.google.firebase.auth.AuthCredential;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.EmailAuthProvider;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.auth.SignInMethodQueryResult;
 import com.mapbox.android.core.location.LocationEngine;
 import com.mapbox.android.core.location.LocationEngineListener;
 import com.mapbox.android.core.location.LocationEnginePriority;
 import com.mapbox.android.core.location.LocationEngineProvider;
 import com.mapbox.android.core.permissions.PermissionsListener;
 import com.mapbox.android.core.permissions.PermissionsManager;
+import com.mapbox.geojson.Feature;
+
+
+import com.mapbox.geojson.FeatureCollection;
+import com.mapbox.geojson.Point;
 import com.mapbox.mapboxsdk.Mapbox;
+import com.mapbox.mapboxsdk.annotations.MarkerViewOptions;
 import com.mapbox.mapboxsdk.camera.CameraUpdateFactory;
 import com.mapbox.mapboxsdk.geometry.LatLng;
 import com.mapbox.mapboxsdk.location.modes.CameraMode;
@@ -21,11 +47,30 @@ import com.mapbox.mapboxsdk.maps.MapView;
 import com.mapbox.mapboxsdk.maps.MapboxMap;
 import com.mapbox.mapboxsdk.maps.OnMapReadyCallback;
 import com.mapbox.mapboxsdk.plugins.locationlayer.LocationLayerPlugin;
+import com.mapbox.mapboxsdk.style.layers.LineLayer;
 
+import com.mapbox.mapboxsdk.style.light.Position;
+import com.mapbox.mapboxsdk.style.sources.GeoJsonSource;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+
+import java.net.URISyntaxException;
+import java.net.URL;
+import java.time.LocalDate;
+import java.time.ZoneId;
+import java.time.format.DateTimeFormatter;
+import java.util.Date;
 import java.util.List;
 
+import timber.log.Timber;
+
+
 public class MainActivity extends AppCompatActivity implements OnMapReadyCallback, LocationEngineListener, PermissionsListener {
-    private String tag = "MainActivity";
     private MapView mapView;
     private MapboxMap map;
 
@@ -33,6 +78,21 @@ public class MainActivity extends AppCompatActivity implements OnMapReadyCallbac
     private LocationEngine locationEngine;
     private LocationLayerPlugin locationLayerPlugin;
     private Location originLocation;
+
+    private final String tag = "MainActivity";
+
+    Date c = Calendar.getInstance().getTime();
+
+    SimpleDateFormat df = new SimpleDateFormat("yyyy/MM/dd");
+    private String downloadDate = df.format(c);
+
+
+  //  private String downloadDate = dtf.format(now).toString();// Format: YYYY/MM/DD
+
+    private final String preferencesFile = "MyPrefsFile"; // for storing preferences
+
+    private FirebaseAuth mAuth;
+    private static final String TAG = "MainActivity";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -44,21 +104,125 @@ public class MainActivity extends AppCompatActivity implements OnMapReadyCallbac
         mapView = findViewById(R.id.mapboxMapView);
         mapView.onCreate(savedInstanceState);
         mapView.getMapAsync(this);
+
+        mAuth = FirebaseAuth.getInstance();
+
     }
+
+
+
 
     @Override
     public void onMapReady(MapboxMap mapboxMap) {
+
         if (mapboxMap == null) {
             Log.d(tag, "[onMapReady] mapBox is null");
         } else {
+
+
             map = mapboxMap;
 // Set user interface options
             map.getUiSettings().setCompassEnabled(true);
             map.getUiSettings().setZoomControlsEnabled(true);
 // Make location information available
             enableLocation();
+
+            String url =  "http://homepages.inf.ed.ac.uk/stg/coinz/" + downloadDate + "/coinzmap.geojson";
+            new DownloadFileTask().execute(url);
+
+            Log.d(tag,"geojson file start downloading.");
+
+
+            Log.d(tag,"gwojson string got");
+            try {
+                URL geoJsonUrl = new URL("http://homepages.inf.ed.ac.uk/stg/coinz/" + downloadDate + "/coinzmap.geojson");
+
+                GeoJsonSource geoJsonSource = new GeoJsonSource("geojson-source", geoJsonUrl);
+                mapboxMap.addSource(geoJsonSource);
+
+                Log.d(tag,"source added");
+                FeatureCollection featureCollection = FeatureCollection.fromJson(url);
+
+                List<Feature> features = featureCollection.features();
+
+                Log.d(tag,"feature collection got");
+
+                for (Feature f : features) {
+                    if (f.geometry() instanceof Point) {
+                        Location coordinates = ((Location) ((Point) f.geometry()).coordinates());
+                        mapboxMap.addMarker(
+                                new MarkerViewOptions().position(new
+                                        LatLng(coordinates.getLatitude(),
+                                        coordinates.getLongitude()))
+                        );
+                    }
+                }
+            } catch (MalformedURLException e) {
+                e.printStackTrace();
+            }
+
+
+            //    URL geoJsonUrl = new URL("http://homepages.inf.ed.ac.uk/stg/coinz/" + downloadDate + "/coinzmap.geojson");
+
+//            try {
+//                DownloadFileTask dl = new DownloadFileTask();
+//                String geoJsonUrl = dl.doInBackground(url);
+//      //         Uri geoJsonUrl = Uri.parse(new java.net.URI("http://homepages.inf.ed.ac.uk/stg/coinz/" + downloadDate + "/coinzmap.geojson").toString());
+//
+//                try {
+//                   // String sUrl = getStringFromFile(geoJsonUrl,getApplicationContext());
+//                    GeoJsonSource geoJsonSource = new GeoJsonSource("geojson-source", geoJsonUrl);
+//                    mapboxMap.addSource(geoJsonSource);
+//
+//                    Log.d(tag,"source added");
+//                    FeatureCollection featureCollection = FeatureCollection.fromJson(geoJsonUrl);
+//
+//                    List<Feature> features = featureCollection.features();
+//
+//                    Log.d(tag,"feature collection got");
+//
+//                    for (Feature f : features) {
+//                        if (f.geometry() instanceof Point) {
+//                            Location coordinates = ((Location) ((Point) f.geometry()).coordinates());
+//                            mapboxMap.addMarker(
+//                                    new MarkerViewOptions().position(new
+//                                            LatLng(coordinates.getLatitude(),
+//                                            coordinates.getLongitude()))
+//                            );
+//                        }
+//                    }
+//                } catch (Exception e) {
+//                    e.printStackTrace();
+//                }
+//            } catch (URISyntaxException e) {
+//                e.printStackTrace();
+//            }
+            Log.d(tag,"geojson rendered.");
         }
     }
+
+
+//    private static String convertStreamToString(InputStream is) throws Exception {
+//
+//        BufferedReader reader = new BufferedReader(new InputStreamReader(is));
+//
+//        StringBuilder sb = new StringBuilder();
+//        String line = null;
+//        while ((line = reader.readLine()) != null) {
+//            sb.append(line).append("\n");
+//        }
+//        reader.close();
+//        return sb.toString();
+//    }
+//
+//    public static String getStringFromFile(Uri fileUri, Context context) throws Exception {
+//        InputStream fin = context.getContentResolver().openInputStream(fileUri);
+//
+//        String ret = convertStreamToString(fin);
+//
+//        fin.close();
+//        return ret;
+//    }
 
     private void enableLocation() {
         if (PermissionsManager.areLocationPermissionsGranted(this)) {
@@ -72,7 +236,8 @@ public class MainActivity extends AppCompatActivity implements OnMapReadyCallbac
         }
     }
 
-    @SuppressWarnings("MissingPermission") private void initializeLocationEngine() {
+    @SuppressWarnings("MissingPermission")
+    private void initializeLocationEngine() {
         locationEngine = new LocationEngineProvider(this) .obtainBestLocationEngineAvailable();
         locationEngine.setInterval(5000); // preferably every 5 seconds
         locationEngine.setFastestInterval(1000); // at most every second
@@ -87,7 +252,8 @@ public class MainActivity extends AppCompatActivity implements OnMapReadyCallbac
         }
     }
 
-    @SuppressWarnings("MissingPermission") private void initializeLocationLayer() {
+    @SuppressWarnings("MissingPermission")
+    private void initializeLocationLayer() {
         if (mapView == null) {
             Log.d(tag, "mapView is null");
         } else {
@@ -141,9 +307,143 @@ public class MainActivity extends AppCompatActivity implements OnMapReadyCallbac
     @Override
     protected void onStart() {
         super.onStart();
+
+        // Restore preferences
+        SharedPreferences settings = getSharedPreferences(preferencesFile, Context.MODE_PRIVATE);
+        // use ”” as the default value (this might be the first time the app is run)
+//        downloadDate = settings.getString("lastDownloadDate", "");
+        Log.d(tag, "[onStart] Recalled lastDownloadDate is " + downloadDate );
+
         mapView.onStart();
+        if(locationEngine != null){
+
+            try {
+                locationEngine.requestLocationUpdates();
+            } catch(SecurityException ignored) {}
+            locationEngine.addLocationEngineListener(this);
+        }
+    }
+
+
+    @Override
+    public void onStop() {
+        super.onStop();
+        Log.d(tag, "[onStop] Storing lastDownloadDate of " + downloadDate);
+        // All objects are from android.context.Context
+        SharedPreferences settings = getSharedPreferences(preferencesFile,
+                Context.MODE_PRIVATE);
+        // We need an Editor object to make preference changes.
+        SharedPreferences.Editor editor = settings.edit();
+        editor.putString("lastDownloadDate", downloadDate);
+        Log.d(tag,downloadDate);
+        Log.d(tag,"download date updated");
+        // Apply the edits!
+        editor.apply();
+
+        mapView.onStop();
+        if(locationEngine != null){
+            locationEngine.removeLocationEngineListener(this);
+            locationEngine.removeLocationUpdates();
+        }
+    }
+
+    public void sendSignInLink(String email, ActionCodeSettings actionCodeSettings) {
+
+        FirebaseAuth auth = FirebaseAuth.getInstance();
+        auth.sendSignInLinkToEmail(email, actionCodeSettings)
+                .addOnCompleteListener(new OnCompleteListener<Void>() {
+                    @Override
+                    public void onComplete(@NonNull Task<Void> task) {
+                        if (task.isSuccessful()) {
+                            Log.d(TAG, "Email sent.");
+                        }
+                    }
+                });
+    }
+
+
+    public void buildActionCodeSettings() {
+
+        ActionCodeSettings actionCodeSettings =
+                ActionCodeSettings.newBuilder()
+                        // URL you want to redirect back to. The domain (www.example.com) for this
+                        // URL must be whitelisted in the Firebase Console.
+                        .setUrl("https://www.example.com/finishSignUp?cartId=1234")
+                        // This must be true
+                        .setHandleCodeInApp(true)
+                        .setIOSBundleId("com.example.ios")
+                        .setAndroidPackageName(
+                                "com.example.android",
+                                true, /* installIfNotAvailable */
+                                "12"    /* minimumVersion */)
+                        .build();
+    }
+
+
+    public void differentiateLink(String email) {
+        FirebaseAuth auth = FirebaseAuth.getInstance();
+
+        auth.fetchSignInMethodsForEmail(email)
+                .addOnCompleteListener(new OnCompleteListener<SignInMethodQueryResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<SignInMethodQueryResult> task) {
+                        if (task.isSuccessful()) {
+                            SignInMethodQueryResult result = task.getResult();
+                            List<String> signInMethods = result.getSignInMethods();
+                            if (signInMethods.contains(EmailAuthProvider.EMAIL_PASSWORD_SIGN_IN_METHOD)) {
+                                // User can sign in with email/password
+                            } else if (signInMethods.contains(EmailAuthProvider.EMAIL_LINK_SIGN_IN_METHOD)) {
+                                // User can sign in with email/link
+                            }
+                        } else {
+                            Log.e(TAG, "Error getting sign in methods for user", task.getException());
+                        }
+                    }
+                });
+    }
+
+    public void signOut() {
+        FirebaseAuth.getInstance().signOut();
     }
 
 
 
+
+    @SuppressLint("StaticFieldLeak")
+    private class DownloadFileTask extends AsyncTask<String, Void, String> {
+        @Override
+        protected String doInBackground(String... urls) { try {
+            return loadFileFromNetwork(urls[0]); } catch (IOException e) {
+            return "Unable to load content. Check your network connection"; }
+        }
+        private String loadFileFromNetwork(String urlString) throws IOException {
+            return readStream(downloadUrl(new URL(urlString)));
+        }
+
+        // Given a string representation of a URL, sets up a connection and gets an input stream.
+        private InputStream downloadUrl(URL url) throws IOException {
+            HttpURLConnection conn = (HttpURLConnection) url.openConnection();
+            conn.setReadTimeout(10000); // milliseconds
+            conn.setConnectTimeout(15000); // milliseconds
+            conn.setRequestMethod("GET");
+            conn.setDoInput(true);
+            conn.connect();
+            return conn.getInputStream();
+        }
+
+        @NonNull
+        private String readStream(InputStream stream)
+                throws IOException {
+            // Read input from stream, build result as a string
+            return null;
+        }
+
+        @Override
+        protected void onPostExecute(String result) {
+            super.onPostExecute(result);
+            DownloadCompleteRunner.downloadComplete(result);
+        }
+    }
 }
+
+
