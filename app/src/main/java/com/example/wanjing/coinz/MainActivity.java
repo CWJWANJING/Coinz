400b036495adaeabd9573e1b3d69a03857c91259
CWJWANJING <wanjingchencwj@gmail.com>
2018-12-07 09:31:12 +0000

    Added collecting coins operations in onLocationchange, but I need to figure out how to get mAuth from Login page, as currently, it's null in main activity.

--- app/src/main/java/com/example/wanjing/coinz/MainActivity.java
+++ app/src/main/java/com/example/wanjing/coinz/MainActivity.java
@@ -14,11 +14,15 @@ import android.net.Uri;
 import android.os.AsyncTask;
 import android.os.Handler;
 import android.support.annotation.NonNull;
+import android.support.design.widget.FloatingActionButton;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.Toolbar;
 import android.util.Log;
 import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
 
 import com.google.android.gms.common.util.IOUtils;
@@ -31,6 +35,10 @@ import com.google.firebase.auth.EmailAuthProvider;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
 import com.google.firebase.auth.SignInMethodQueryResult;
+import com.google.firebase.firestore.DocumentReference;
+import com.google.firebase.firestore.DocumentSnapshot;
+import com.google.firebase.firestore.FirebaseFirestore;
+import com.google.firebase.firestore.FirebaseFirestoreSettings;
 import com.mapbox.android.core.location.LocationEngine;
 import com.mapbox.android.core.location.LocationEngineListener;
 import com.mapbox.android.core.location.LocationEnginePriority;
@@ -77,9 +85,17 @@ import java.net.URL;
 import java.time.LocalDate;
 import java.time.ZoneId;
 import java.time.format.DateTimeFormatter;
+import java.util.Arrays;
+import java.util.Comparator;
 import java.util.Date;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 import java.util.concurrent.ExecutionException;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
 
 import timber.log.Timber;
 
@@ -87,24 +103,41 @@ import static com.mapbox.mapboxsdk.style.layers.PropertyFactory.lineColor;
 
 
 public class MainActivity extends AppCompatActivity implements OnMapReadyCallback, LocationEngineListener, PermissionsListener {
+    // a general tag
+    private final String tag = "MainActivity";
+
+    // this is for the mapbox
     private MapView mapView;
     private MapboxMap map;
 
+    // this is for getting the user location
     private PermissionsManager permissionsManager;
     private LocationEngine locationEngine;
     private LocationLayerPlugin locationLayerPlugin;
     private Location originLocation;
 
-    private final String tag = "MainActivity";
-
+    // this is for getting the current date
     Date c = Calendar.getInstance().getTime();
-
     SimpleDateFormat df = new SimpleDateFormat("yyyy/MM/dd");
     private String currentDate = df.format(c);
-    private String downloadDate = "";
 
+    // for shared reference file
+    private String downloadDate = "";
     private final String preferencesFile = "MyPrefsFile"; // for storing preferences
 
+    // for store data in firestore
+    private FirebaseAuth mAuth;
+    private FirebaseFirestore firestore;
+    private DocumentReference firestoreBank;
+    private static final String t = "Bank";
+    private static final String COLLECTION_KEY = "Bank";
+
+    private EditText memail;
+    private Button mstore;
+    private EditText mCoins;
+
+    private static final String email = "Name";
+    private static final String coins = "Text";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -119,9 +152,60 @@ public class MainActivity extends AppCompatActivity implements OnMapReadyCallbac
         mapView.onCreate(savedInstanceState);
         mapView.getMapAsync(this);
 
+        // firestore
+        firestore = FirebaseFirestore.getInstance();
+        FirebaseFirestoreSettings settings = new FirebaseFirestoreSettings.Builder().setTimestampsInSnapshotsEnabled(true)
+                .build();
+        firestore.setFirestoreSettings(settings);
+
+        memail = findViewById(R.id.userEmail);
+        mstore = findViewById(R.id.store);
+        mCoins = findViewById(R.id.coins);
+
+        firestoreBank = firestore.collection(COLLECTION_KEY).document();
+
+        mstore.setOnClickListener(view -> store());
+
+        // I tried to collect the coins automatically, but for some reason, it fails:
+//        String userID = mAuth.getCurrentUser().getUid();
+//        Float defalt = 0f;
+//        Map<String,Float> userMap = new HashMap<>();
+//        userMap.put(user,defalt);
+//        firestoreBank.set(userMap);
+    }
+
+    private void store() {
+        // create a message of the form { ”email”: str1, ”coins”: str2 }
+        Map<String, String> bank = new HashMap<>();
+        bank.put(email, memail.getText().toString());
+        bank.put(coins, mCoins.getText().toString());
+        // send the message and listen for success or failure
+        firestoreBank.set(bank)
+                .addOnSuccessListener(v -> Toast.makeText(getApplicationContext(),
+
+                        "Message sent!",
+                        Toast.LENGTH_SHORT).show())
+                .addOnFailureListener(e -> {
+                    Log.e(t, e.getMessage());
+                });
 
     }
 
+
+    // downloading the file from url
+    String url = "http://homepages.inf.ed.ac.uk/stg/coinz/" + currentDate + "/coinzmap.geojson";
+    DownloadFileTask dlf = new DownloadFileTask();
+    String result;
+
+    {
+        try {
+            result = dlf.execute(url).get();
+        } catch (ExecutionException e) {
+            e.printStackTrace();
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+    }
 
     @Override
     public void onMapReady(MapboxMap mapboxMap) {
@@ -132,50 +216,58 @@ public class MainActivity extends AppCompatActivity implements OnMapReadyCallbac
 
 
             map = mapboxMap;
-// Set user interface options
+            // Set user interface options
             map.getUiSettings().setCompassEnabled(true);
             map.getUiSettings().setZoomControlsEnabled(true);
-// Make location information available
+            // Make location information available
             enableLocation();
-
 
             Log.d(tag, "geojson file start downloading.");
 
             try {
 
-                String url = "http://homepages.inf.ed.ac.uk/stg/coinz/" + currentDate + "/coinzmap.geojson";
-                DownloadFileTask dlf = new DownloadFileTask();
-                String result = dlf.execute(url).get();
-
                 GeoJsonSource source = new GeoJsonSource("geojson", result);
                 mapboxMap.addSource(source);
 
-
+                // prepare icons for each currency
                 IconFactory iconFactory = IconFactory.getInstance(MainActivity.this);
                 Icon icon0 = iconFactory.fromResource(R.drawable.icon0);
                 Icon icon1 = iconFactory.fromResource(R.drawable.icon1);
                 Icon icon2 = iconFactory.fromResource(R.drawable.icon2);
                 Icon icon3 = iconFactory.fromResource(R.drawable.icon3);
 
-
+                // get features from geojson string
                 FeatureCollection featureCollection = FeatureCollection.fromJson(result);
                 List<Feature> features = featureCollection.features();
 
+                // get the currency rates from geojson file
+                JSONObject myObject = new JSONObject(result);
+                JSONObject json_rates = myObject.getJSONObject("rates");
+                String rS = json_rates.get("SHIL").toString();
+                String rP = json_rates.get("PENY").toString();
+                String rQ = json_rates.get("QUID").toString();
+                String rD = json_rates.get("QUID").toString();
+
+
+                float rateq = Float.parseFloat(rQ) / Float.parseFloat(rD);
+                float rates = Float.parseFloat(rS) / Float.parseFloat(rD);
+                float ratep = Float.parseFloat(rP) / Float.parseFloat(rD);
+                // loop through feature collection to add each feature with different icons according to their currencies
                 for (Feature f : features) {
                     if (f.geometry() instanceof Point) {
                         if (f.getStringProperty("currency").equals("QUID")){
-                            mapboxMap.addMarker(new MarkerViewOptions()
-                                    .position(new LatLng(((Point) f.geometry()).latitude(), ((Point) f.geometry()).longitude()))
-                                    .icon(icon0)
-                                    .snippet(f.getStringProperty("value") + " " + f.getStringProperty("currency"))
-                                    .title(f.getStringProperty("id"))
-                            );
+                                mapboxMap.addMarker(new MarkerViewOptions()
+                                        .position(new LatLng(((Point) f.geometry()).latitude(), ((Point) f.geometry()).longitude()))
+                                        .icon(icon0)
+                                        .snippet(Float.toString(rateq*Float.parseFloat(f.getStringProperty("value"))))
+                                        .title(f.getStringProperty("id"))
+                                );
                         }
                         else if (f.getStringProperty("currency").equals("SHIL")){
                             mapboxMap.addMarker(new MarkerViewOptions()
                                     .position(new LatLng(((Point) f.geometry()).latitude(), ((Point) f.geometry()).longitude()))
                                     .icon(icon1)
-                                    .snippet(f.getStringProperty("value") + " " + f.getStringProperty("currency"))
+                                    .snippet(Float.toString(rates*Float.parseFloat(f.getStringProperty("value"))))
                                     .title(f.getStringProperty("id"))
                             );
                         }
@@ -183,7 +275,7 @@ public class MainActivity extends AppCompatActivity implements OnMapReadyCallbac
                             mapboxMap.addMarker(new MarkerViewOptions()
                                     .position(new LatLng(((Point) f.geometry()).latitude(), ((Point) f.geometry()).longitude()))
                                     .icon(icon2)
-                                    .snippet(f.getStringProperty("value") + " " + f.getStringProperty("currency"))
+                                    .snippet(Float.toString(ratep*Float.parseFloat(f.getStringProperty("value"))))
                                     .title(f.getStringProperty("id"))
                             );
                         }
@@ -191,35 +283,18 @@ public class MainActivity extends AppCompatActivity implements OnMapReadyCallbac
                             mapboxMap.addMarker(new MarkerViewOptions()
                                     .position(new LatLng(((Point) f.geometry()).latitude(), ((Point) f.geometry()).longitude()))
                                     .icon(icon3)
-                                    .snippet(f.getStringProperty("value") + " " + f.getStringProperty("currency"))
+                                    .snippet(Float.toString(ratep*Float.parseFloat(f.getStringProperty("value"))))
                                     .title(f.getStringProperty("id"))
                             );
                         }
                     }
                 }
-            } catch (InterruptedException e) {
-                e.printStackTrace();
-            } catch (ExecutionException e) {
+            } catch (JSONException e) {
                 e.printStackTrace();
             }
             Log.d(tag, "geojson rendered.");
-
         }
     }
-//                FeatureCollection featureCollection = FeatureCollection.fromJson(result);
-//
-//                GeoJsonSource geoJsonSource = new GeoJsonSource("marker-source", featureCollection);
-//                mapboxMap.addSource(geoJsonSource);
-//
-//                Bitmap icon = BitmapFactory.decodeResource(
-//                        MainActivity.this.getResources(), R.drawable.ic_maneuver_fork);
-//
-//                mapboxMap.addImage("my-marker-image", icon);
-//
-//                SymbolLayer markers = new SymbolLayer("marker-layer", "marker-source")
-//                        .withProperties(PropertyFactory.iconImage("my-marker-image"));
-//                mapboxMap.addLayer(markers);
-
 
 
     private void enableLocation() {
@@ -280,6 +355,90 @@ public class MainActivity extends AppCompatActivity implements OnMapReadyCallbac
             Log.d(tag, "[onLocationChanged] location is not null");
             originLocation = location;
             setCameraPosition(location);
+
+            // get features from geojson string
+            FeatureCollection featureCollection = FeatureCollection.fromJson(result);
+            List<Feature> features = featureCollection.features();
+
+            // calculate the distances between each feature and the current location
+            Float[] distanceInMeters = new Float[features.size()];
+            for (int i = 0; i < features.size(); i++){
+                Location loc = new Location("");
+                loc.setLatitude(((Point) features.get(i).geometry()).latitude());
+                loc.setLongitude(((Point) features.get(i).geometry()).longitude());
+                if(originLocation != null){
+                    distanceInMeters[i] = originLocation.distanceTo(loc);
+                }
+            }
+
+            // sort the array which store the distances
+            Arrays.sort(distanceInMeters, new Comparator<Float>() {
+                @Override
+                public int compare(Float o1, Float o2) {
+                    if (o1 == null && o2 == null) {
+                        return 0;
+                    }
+                    if (o1 == null) {
+                        return 1;
+                    }
+                    if (o2 == null) {
+                        return -1;
+                    }
+                    return o1.compareTo(o2);
+                }});
+
+            if (mAuth != null) {
+                Log.d(t,"mAuth is not null");
+                String userID = mAuth.getCurrentUser().getUid();
+                Map<String, Float> userMap = new HashMap<>();
+
+                for (Feature f : features) {
+                    // get the index of closest marker
+                    // if user is close enough to a marker, then the value of coins is shown
+                    if (distanceInMeters[0] != null && distanceInMeters[0] < 25) {
+                        float min = distanceInMeters[0];
+                        List<Float> abcd = Arrays.asList(distanceInMeters);
+                        int minindex = abcd.indexOf(min);
+                        if (f.getStringProperty("currency").equals("QUID")) {
+                            String coins = features.get(minindex).getStringProperty("value");
+                            Float coinint = Float.parseFloat(coins);
+                            userMap.put(userID, coinint);
+                            firestoreBank.set(userMap);
+                            Toast.makeText(getApplicationContext(),
+                                    "QUID coins collected!",
+                                    Toast.LENGTH_LONG).show();
+//                        int rate = Integer.parseInt(rQ) / Integer.parseInt(rD);
+//                        int c = Integer.parseInt(features.get(minindex).getStringProperty("value"))*rate;
+//                        String s = Integer.toString(c);
+                        }
+                        else if(f.getStringProperty("currency").equals("SHIL")){
+                            String coins = features.get(minindex).getStringProperty("value");
+                            Float coinint = Float.parseFloat(coins);
+                            userMap.put(userID, coinint);
+                            firestoreBank.set(userMap);
+                            Toast.makeText(getApplicationContext(),
+                                    "SHIL coins collected!",
+                                    Toast.LENGTH_LONG).show();
+                        }else if (f.getStringProperty("currency").equals("PENY")){
+                            String coins = features.get(minindex).getStringProperty("value");
+                            Float coinint = Float.parseFloat(coins);
+                            userMap.put(userID, coinint);
+                            firestoreBank.set(userMap);
+                            Toast.makeText(getApplicationContext(),
+                                    "PENY coins collected!",
+                                    Toast.LENGTH_LONG).show();
+                        }else if (f.getStringProperty("currency").equals("DOLR")){
+                            String coins = features.get(minindex).getStringProperty("value");
+                            Float coinint = Float.parseFloat(coins);
+                            userMap.put(userID, coinint);
+                            firestoreBank.set(userMap);
+                            Toast.makeText(getApplicationContext(),
+                                    "DOLR coins collected!",
+                                    Toast.LENGTH_LONG).show();
+                        }
+                    }
+                }// end of for loop
+            }else{Log.d(t,"mAuth is null");}
         }
     }
 
@@ -299,7 +458,7 @@ public class MainActivity extends AppCompatActivity implements OnMapReadyCallbac
         if (granted) {
             enableLocation();
         } else {
-// Open a dialogue with the user
+        // Open a dialogue with the user
         }
     }
 
