7f4de624d3de0d2bba813423412b889594c5886f
CWJWANJING <wanjingchencwj@gmail.com>
2018-12-10 12:20:29 +0000

    Theme changed in setting activity

--- app/src/main/java/com/example/wanjing/coinz/Setting.java
+++ app/src/main/java/com/example/wanjing/coinz/Setting.java
@@ -1,6 +1,8 @@
 package com.example.wanjing.coinz;
 
 import android.content.Intent;
+import android.content.SharedPreferences;
+import android.preference.PreferenceManager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
@@ -16,12 +18,35 @@ import java.util.Map;
 
 public class Setting extends AppCompatActivity {
 
+
+    private Button btn;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+        setTheme(getFlag() ? R.style.AppTheme: R.style.coinz);
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_setting);
+
+        btn = (Button) findViewById(R.id.theme);
+        btn.setOnClickListener((view) -> {
+            saveFlag(!getFlag());
+
+            Intent intent = new Intent(Setting.this,Setting.class);
+            startActivity(intent);
+            finish();
+        });
     }
 
+    private void saveFlag(boolean flag){
+        SharedPreferences preferences = PreferenceManager.getDefaultSharedPreferences(this);
+        SharedPreferences.Editor editor = preferences.edit();
+        editor.putBoolean("green",flag);
+        editor.commit();
+    }
+
+    private boolean getFlag(){
+        SharedPreferences preferences = PreferenceManager.getDefaultSharedPreferences(this);
+        return preferences.getBoolean("green",false);
+    }
 
     public void firebaseLogin(View view){
         Intent startNewActivity = new Intent(this,firebaseLogin.class);
@@ -48,8 +73,4 @@ public class Setting extends AppCompatActivity {
         startActivity(startNewActivity);
     }
 
-    public void Theme(View view){
-        Intent startNewActivity = new Intent(this,Theme.class);
-        startActivity(startNewActivity);
-    }
 }
